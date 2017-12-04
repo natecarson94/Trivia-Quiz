d8b7515851fff3190ef6c066cbad803cbafb5535
natecarson94 <33134997+natecarson94@users.noreply.github.com>
2017-12-04 12:07:02 -0500

    Added Styling
    Changed fonts, backgrounds, and positioning.

--- Quiz/script.js
+++ Quiz/script.js
@@ -8,6 +8,7 @@ function append(parent, el){
   return parent.appendChild(el);
 }
 
+//Get Quiz Data
 fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple')
   .then(response => response.json())
   .then(data => {
@@ -16,28 +17,33 @@ fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple')
     let questions = api.map(key =>{
       return key.question;
     })
-
+//assign all answers to an array
     let answers = api.map(key =>{
       let allAnswers= key.incorrect_answers.push(key.correct_answer);
       return key.incorrect_answers;
     })
+// Establish the correct answer
     let correct_answers = api.map(key=>{
         return key.correct_answer;
     })
-
+//create the function to shuffle the answers
     function shuffleArray(array) {
       for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
       }
     }
+//Create a Question header and a ul.
+//Shuffle the answers
     for(i=0; i<5; i++){
       let h2 = createNode('h2');
       let ul = createNode('ul');
       h2.innerHTML = questions[i];
       shuffleArray(answers[i]);
 
-
+//loop through answers
+//create li for each answer
+//create radio button for each answer
       for(j=0; j<4; j++){
 
         let li= createNode('li');
@@ -51,10 +57,18 @@ fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple')
           append(li,input);
           append(ul,li);
           append(h2,ul);
-          //console.log(input);
+
         }
         append(quiz,h2);
     }
+    let input=createNode('input');
+    input.id="submitButton";
+    input.type="submit";
+    input.name="mysubmit";
+    input.value="Submit";
+
+    append(quiz,input);
+//Calculate your score
     quiz.onsubmit = function(){
       let scoreKeeper = 0;
       let key = ['incorrect','incorrect','incorrect','incorrect','incorrect'];
@@ -67,7 +81,7 @@ fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple')
       }
       }
 alert(`You got ${scoreKeeper} out of 5 correct.
-  
+
 Question 1 was ${key[0]}
 Question 2 was ${key[1]}
 Question 3 was ${key[2]}
